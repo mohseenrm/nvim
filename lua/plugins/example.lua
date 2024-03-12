3f8b5bac06bef283ecc0da66a59a99e64910c46e
Mohseen Mukaddam <mmukaddam@twilio.com>
2024-03-12 16:05:24 -0700

    [update] saving progress

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -400,4 +400,29 @@ return {
       save_empty = false, -- don't save if there are no open file buffers
     } },
   },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["javascript"] = { "prettier" },
+        ["javascriptreact"] = { "prettier" },
+        ["typescript"] = { "prettier" },
+        ["typescriptreact"] = { "prettier" },
+        ["vue"] = { "prettier" },
+        ["css"] = { "prettier" },
+        ["scss"] = { "prettier" },
+        ["less"] = { "prettier" },
+        ["html"] = { "prettier" },
+        ["json"] = { "prettier" },
+        ["jsonc"] = { "prettier" },
+        ["yaml"] = { "prettier" },
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+        ["graphql"] = { "prettier" },
+        ["handlebars"] = { "prettier" },
+      },
+    },
+  }
 }
