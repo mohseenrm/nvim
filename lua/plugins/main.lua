6a1945f6eadfe5e3920bef76bd99d51b37e6a884
mohseenrm <mohseenmukaddam6@gmail.com>
2024-05-30 07:52:02 -0700

    [update] saving progress

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -7,16 +7,6 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- change trouble config
-  {
-    "folke/trouble.nvim",
-    -- opts will be merged with the parent spec
-    opts = { use_diagnostic_signs = true },
-  },
-
-  -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
-
   -- add symbols-outline
   {
     "simrat39/symbols-outline.nvim",
