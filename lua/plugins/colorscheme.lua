7288d4843c41429b38a89af6b6ef2f4253e55a82
mohseenrm <mohseenmukaddam6@gmail.com>
2024-06-03 13:14:34 -0700

    [update] saving progress

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,20 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = { style = "night" },
+  -- add tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
+    enabled = true,
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    lazy = true,
+  },
+  -- Configure LazyVim to load prefered colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
 }
