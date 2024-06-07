d8a80cd24433c4c1d40bdbe348d4717ba76953c7
mohseenrm <mohseenmukaddam6@gmail.com>
2024-06-07 15:48:41 -0700

    [update] saving progress

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -95,7 +95,18 @@ require("gitsigns").setup({
   },
 })
 
-require("lualine").setup({
+local function show_macro_recording()
+  local recording_register = vim.fn.reg_recording()
+  if recording_register == "" then
+    return ""
+  else
+    return "Recording @" .. recording_register
+  end
+end
+
+local lualine = require("lualine")
+
+lualine.setup({
   options = {
     icons_enabled = true,
     theme = "auto",
@@ -116,7 +127,7 @@ require("lualine").setup({
   },
   sections = {
     lualine_a = { "mode" },
-    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_b = { "branch", "diff", "diagnostics", { "macro-recording", fmt = show_macro_recording } },
     lualine_c = { { "filename", file_status = true, path = 1 } },
     lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_y = { "progress" },
@@ -134,4 +145,35 @@ require("lualine").setup({
   winbar = {},
   inactive_winbar = {},
   extensions = {},
+})
+
+-- vim.cmd([[ autocmd RecordingEnter * set cmdheight=1 ]])
+-- vim.cmd([[ autocmd RecordingLeave * set cmdheight=0 ]])
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function()
+    lualine.refresh({
+      place = { "statusline" },
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  callback = function()
+    -- This is going to seem really weird!
+    -- Instead of just calling refresh we need to wait a moment because of the nature of
+    -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+    -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+    -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+    -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+    local timer = vim.loop.new_timer()
+    timer:start(
+      50,
+      0,
+      vim.schedule_wrap(function()
+        lualine.refresh({
+          place = { "statusline" },
+        })
+      end)
+    )
+  end,
 })
--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -200,56 +200,6 @@ return {
         --[[add your custom lualine config here]]
       }
     end,
-    config = function()
-      local function show_macro_recording()
-        local recording_register = vim.fn.reg_recording()
-        if recording_register == "" then
-          return ""
-        else
-          return "Recording @" .. recording_register
-        end
-      end
-      require("lualine").setup({
-        sections = {
-          lualine_b = {
-            {
-              "macro-recording",
-              fmt = show_macro_recording,
-            },
-          },
-        }
-      })
-      vim.cmd [[ autocmd RecordingEnter * set cmdheight=1 ]]
-      vim.cmd [[ autocmd RecordingLeave * set cmdheight=0 ]]
-      vim.api.nvim_create_autocmd("RecordingEnter", {
-        callback = function()
-          require("lualine").refresh({
-            place = { "statusline" },
-          })
-        end,
-      })
-
-      vim.api.nvim_create_autocmd("RecordingLeave", {
-        callback = function()
-          -- This is going to seem really weird!
-          -- Instead of just calling refresh we need to wait a moment because of the nature of
-          -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
-          -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
-          -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
-          -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
-          local timer = vim.loop.new_timer()
-          timer:start(
-            50,
-            0,
-            vim.schedule_wrap(function()
-              require("lualine").refresh({
-                place = { "statusline" },
-              })
-            end)
-          )
-        end,
-      })
-    end,
   },
 
   { "nvim-tree/nvim-web-devicons", lazy = true },
